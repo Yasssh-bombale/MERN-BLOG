d1ddc2a1d29ea71dc73831d4bb5867318fed7e02
Yash Vijay Bombale <yashbombale@gmail.com>
2024-01-24 23:21:42 +0530

    complete header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,13 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import DashBoard from "./pages/DashBoard";
 import Projects from "./pages/Projects";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Button, Navbar, TextInput } from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+import { BsFillMoonStarsFill } from "react-icons/bs";
+const Header = () => {
+  const path = useLocation().pathname;
+  return (
+    <Navbar className="border-b-2 ">
+      <Link
+        to={"/"}
+        className="whitespace-nowrap self-center text-sm sm:text-lg font-semibold dark:text-white"
+      >
+        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 to-pink-500  text-white rounded-md">
+          Yassshu's
+        </span>
+        Blog
+      </Link>
+
+      <form>
+        <TextInput
+          type="text"
+          placeholder="search..."
+          rightIcon={AiOutlineSearch}
+          className="hidden lg:inline"
+        />
+        <Button color="gray" className="w-12 h-10 lg:hidden" pill>
+          <AiOutlineSearch className="text-xl" />
+        </Button>
+      </form>
+      <div className="flex gap-4 md:order-2">
+        <Button color="gray" className="w-15 h-10 hidden sm:inline" pill>
+          <BsFillMoonStarsFill />
+        </Button>
+        <Link to={"/sign-in"}>
+          <Button gradientDuoTone={"purpleToPink"}>Sign In</Button>
+        </Link>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        {/* Note: Navbar.Link will give error in console because we are using two Link in each other that are not allowed hence we are converting Navbar.Link as={div} */}
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to={"/"}>Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to={"/about"}>About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
+          <Link to={"/projects"}>Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default Header;
